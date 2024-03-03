0da59dde5884fac952c97db4da9b32a1699ff603
eputrap <eputrap@gmail.com>
2024-03-04 05:38:26 +0700

    second commit

---
+++ Creating Database.sql
@@ -0,0 +1,12 @@
+-- dont uses spaces when giving name to the database
+-- ex My App
+-- instead using spaces, recomened to use underscore or capitalize in first word
+-- ex soap_store or DogApp
+create database pet_shop;
+create database chicken_coop;
+create database SlimeStore;
+
+drop database chicken_coop;
+
+
+show databases
